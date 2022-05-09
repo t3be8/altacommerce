fc97a50966a1e30278d58cd7bba4b9c194c7fae6
Rizki Firdaus <rizki.achanation@gmail.com>
2022-05-09 14:12:07 +0700

    update entity using gorm.model

--- entity/models.go
+++ entity/models.go
@@ -1,37 +1,11 @@
 package entity
 
 import (
-	"time"
+	"gorm.io/gorm"
 )
 
-type User struct {
-	ID        uint
-	Name      string
-	Dob       *time.Time
-	Gender    string
-	Email     string
-	Phone     *string
-	Password  string
-	CreatedAt *time.Time
-	UpdatedAt *time.Time
-	Addresses []Address
-}
-
-func (User) TableName() string {
-	return "users"
-}
-
-type Address struct {
-	ID        uint
-	Address   string
-	KodePos   int
-	UserID    uint
-	CreatedAt *time.Time
-	UpdatedAt *time.Time
-}
-
 type Product struct {
-	ID               uint
+	gorm.Model
 	Name             string
 	Description      string
 	Price            int
@@ -40,15 +14,10 @@ type Product struct {
 	Image            string
 	ProducCategoryID uint
 	UserID           uint
-	CreatedAt        *time.Time
-	UpdatedAt        *time.Time
 	ProductCategory  []ProductCategory
 }
 
 type ProductCategory struct {
-	ID        uint
-	Name      string
-	CreatedAt *time.Time
-	UpdatedAt *time.Time
-	DeletedAt *time.Time
+	gorm.Model
+	Name string
 }
