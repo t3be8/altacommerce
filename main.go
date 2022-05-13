6e668d9e61e2f68a0c87e2131e72b5628741ac09
adeeplearn <galang8280@students.amikom.ac.id>
2022-05-13 08:33:30 +0700

    register path to main

--- main.go
+++ main.go
@@ -8,30 +8,38 @@ import (
 	"github.com/labstack/gommon/log"
 	"github.com/t3be8/altacommerce/config"
 	cartController "github.com/t3be8/altacommerce/delivery/controllers/cart"
+	orderController "github.com/t3be8/altacommerce/delivery/controllers/order"
 	productController "github.com/t3be8/altacommerce/delivery/controllers/product"
 	userController "github.com/t3be8/altacommerce/delivery/controllers/user"
 	"github.com/t3be8/altacommerce/delivery/routes"
 	"github.com/t3be8/altacommerce/entity"
 	cartRepo "github.com/t3be8/altacommerce/repository/cart"
+	orderRepo "github.com/t3be8/altacommerce/repository/order"
 	productRepo "github.com/t3be8/altacommerce/repository/product"
 	userRepo "github.com/t3be8/altacommerce/repository/user"
+	"github.com/t3be8/altacommerce/utils"
 )
 
 func main() {
 	conf := config.InitConfig()
 	db := config.InitDB(*conf)
-	db.AutoMigrate(&entity.User{}, &entity.Address{}, &entity.Shipment{}, &entity.OrderDetail{}, &entity.Order{}, &entity.Category{}, &entity.Product{}, &entity.Cart{})
+	snap := utils.InitMidrans()
+	db.AutoMigrate(
+		&entity.User{},
+		&entity.Address{}, &entity.Shipment{}, &entity.OrderDetail{}, &entity.Order{}, &entity.Category{}, &entity.Product{}, &entity.Cart{})
 
 	e := echo.New()
 
 	repoUser := userRepo.New(db)
 	repoProduct := productRepo.New(db)
 	repocart := cartRepo.New(db)
+	repoOrder := orderRepo.New(db)
 
 	controllerUser := userController.New(repoUser, validator.New())
+	controllerOrder := orderController.New(repoOrder, snap, validator.New())
 	controllerProduct := productController.New(repoProduct, validator.New())
 	controllerCart := cartController.New(repocart, validator.New())
-	routes.RegisterPath(e, controllerUser, controllerProduct, controllerCart)
+	routes.RegisterPath(e, controllerUser, controllerProduct, controllerCart, controllerOrder)
 	log.Fatal(e.Start(fmt.Sprintf(":%d", conf.Port)))
 
 }
--- repository/order/interface.go
+++ repository/order/interface.go
@@ -4,6 +4,6 @@ import "github.com/t3be8/altacommerce/entity"
 
 type IOrder interface {
 	CreateOrder(order entity.Order) (entity.Order, error)
-	CancelOrder(order_id int) error
+	CancelOrder(order_id uint) error
 	PayOrder(user_id uint, order_id string) (entity.Order, error)
 }
